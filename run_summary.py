fb69485f5beda75fb7656a3a9e985be74eab4f26
Adam Meyers <meyers@grice.cs.nyu.edu>
2019-09-20 11:56:50 -0400

    typo

--- run_summary.py
+++ run_summary.py
@@ -10,6 +10,6 @@ def main(args):
     input_file = prefix+'.term_instance_map'
     output_file = prefix+'.summary'
     cluster_strategy='big_centroid_max'
-    generate_summaries_from_term_file_map(input_file,output_file,directory,cluster_sample_strategy=cluster_strategy,txt_type=txt_type)
+    generate_summaries_from_term_file_map(input_file,output_file,directory,cluster_sample_strategy=cluster_strategy,txt_file_type=txt_type)
     
 if __name__ == '__main__': sys.exit(main(sys.argv))
